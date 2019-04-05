94892a5c481bd4112099759a26b588e4067e3dd5
Onil Pravasi <onilpravasi@yahoo.com>
2019-04-05 11:42:05 -0500

    correct version

--- B18Android/src/inheritance/Son.java
+++ B18Android/src/inheritance/Son.java
@@ -5,7 +5,13 @@ public class Son extends Father {
 	@Override
 	void playGame()
 	{
+		super.playGame();
 		System.out.println("play cricket");
+	}
+	public static void main(String [] args)
+	{
+		Son son = new Son();
+		son.playGame();
 	}
 
 }
